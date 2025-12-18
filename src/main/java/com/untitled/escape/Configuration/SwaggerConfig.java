84bbcfd24c60f861049dce4e45f9d786fbd4596a
duhawon <duhawon1123@gmail.com>
2025-12-18 21:42:03 +0900

    Swagger 설정

---
+++ src/main/java/com/untitled/escape/Configuration/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.untitled.escape.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(info());
+    }
+    private Info info() {
+        return new Info()
+                .title("Spring Boot Open API with Swagger")
+                .description("description")
+                .version("1.0.0");
+    }
+}
